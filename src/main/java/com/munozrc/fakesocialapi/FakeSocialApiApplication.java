25a8a0556db3895bed52b9376ec9cdf7af1514e8
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-02-19 23:15:06 -0500

    chore: Initialize project structure

---
+++ src/main/java/com/munozrc/fakesocialapi/FakeSocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.munozrc.fakesocialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeSocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeSocialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/munozrc/fakesocialapi/FakeSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munozrc.fakesocialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeSocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
